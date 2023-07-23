b80af96fca7cc5022dda71fb2a37b31d7ef03389
krustnic <avmityakov@gmail.com>
2023-07-23 08:57:57 +0300

    extend logs

--- app.js
+++ app.js
@@ -10,9 +10,16 @@ const app = express()
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
-app.use(morgan('combined', { stream: accessLogStream }))
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]', { stream: accessLogStream }));
+// app.use(morgan('combined', { stream: accessLogStream }))
 
 app.get('/', function (req, res) {
+    res.send('OK');
+});
+
+app.post('/', function (req, res) {
     res.send('OK');
 });
 
